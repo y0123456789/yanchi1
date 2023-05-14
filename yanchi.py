071f350d90f9269ae777e899b2b541467c343871
y0123456789 <52168408+y0123456789@users.noreply.github.com>
2023-05-14 18:30:48 +0800

    Update yanchi.py

--- yanchi.py
+++ yanchi.py
@@ -10,7 +10,7 @@ url = "https://y0123456789.github.io/check-pallas/minified-v3.json"
 response = requests.get(url).json()
 
 # 获取mobileconfig模板文件
-template_url = "https://raw.githubusercontent.com/y0123456789/yang/main/deploy/template.mobileconfig"
+template_url = "https://raw.githubusercontent.com/y0123456789/yanchi/main/template.mobileconfig"
 template = requests.get(template_url).text
 
 # 循环修改模板文件并保存mobileconfig文件
@@ -20,15 +20,18 @@ for name, versions in response.items():
     for version in versions:
         if isinstance(version, dict) and version.get('date') is not None and version.get('delay') is not None and version.get('delay') >= 0:
             delta = datetime.now(timezone.utc) - datetime.fromisoformat(version["date"].replace("Z", "+00:00"))
-            days = delta.days
+            day = datetime.fromisoformat(version["date"].replace("Z", "+00:00")) - datetime.now(timezone.utc)
+            days = day.days
             print(f"Name: {version['name']}, Date: {version['date']}, Delta: {delta}")
 
         else:
             days = 0
 
+
         # 修改mobileconfig文件
         if "delay" in version and version["delay"] >= 0:
             mobileconfig = template.replace("{DELAYPERIOD}", str(version["delay"]))
+            mobileconfig = mobileconfig.replace("{NAME}", str(version["name"]))
 
             # 保存mobileconfig文件
             filename = f"./{version['name']} —剩余{days}天.mobileconfig"
