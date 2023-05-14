f123aa93a920d00e9f29316d35e24ce28ee70225
y0123456789 <52168408+y0123456789@users.noreply.github.com>
2023-05-14 23:49:06 +0800

    Create yanchi.py

---
+++ yanchi.py
@@ -0,0 +1,79 @@
+import json
+import requests
+from datetime import datetime, timezone
+from urllib.parse import urlparse
+import os
+
+
+# 获取json文件
+url = "https://y0123456789.github.io/check-pallas/minified-v3.json"
+response = requests.get(url).json()
+# 删除指定的键值对
+if "iOS (iPhone 14 series)" in response:
+    response.pop("iOS (iPhone 14 series)")
+if "iPadOS (October 2022 models)" in response:
+    response.pop("iPadOS (October 2022 models)")
+if "_date" in response:
+    response.pop("_date")
+if "iOS (all other devices supporting iOS 16)" in response:
+    response["iOS (iPhone 8 - iPhone 14)"] = response.pop("iOS (all other devices supporting iOS 16)")
+if "iOS Legacy v2 (device supporting up to iOS 15)" in response:
+    response["iOS (iPhone 6s - iPhone 7)"] = response.pop("iOS Legacy v2 (device supporting up to iOS 15)")
+if "iPadOS" in response:
+    response["iPadOS (最高iPadOS 16)"] = response.pop("iPadOS")
+if "iPadOS Legacy v2 (devices supporting up to iOS 15)" in response:
+    response["iPadOS (最高iPadOS 15)"] = response.pop("iPadOS Legacy v2 (devices supporting up to iOS 15)")
+if "macOS" in response:
+    response["macOS"] = response.pop("macOS")
+if "tvOS" in response:
+    response["tvOS"] = response.pop("tvOS")
+
+# 删除delay值为0和小于0的数据
+for name, versions in response.items():
+    for version in versions[:]:
+        if isinstance(version, dict) and "delay" in version and version["delay"] <= 0:
+            versions.remove(version)
+# 如果数据为空，则删除对应的键值对
+for name, versions in list(response.items()):
+    if not versions:
+        del response[name]
+# 获取mobileconfig模板文件
+template_url = "https://raw.githubusercontent.com/y0123456789/yanchi/main/template.mobileconfig"
+template = requests.get(template_url).text
+
+# 循环修改模板文件并保存mobileconfig文件
+# 循环获取版本名称和更新时间
+for name, versions in response.items():
+    print(f"{name}:")
+    for version in versions:
+        if isinstance(version, dict) and version.get('date') is not None and version.get('delay') is not None and version.get('delay') >= 0:
+            delta = datetime.now(timezone.utc) - datetime.fromisoformat(version["date"].replace("Z", "+00:00"))
+            day = datetime.fromisoformat(version["date"].replace("Z", "+00:00")) - datetime.now(timezone.utc)
+            days = day.days
+            print(f"Name: {version['name']}, Date: {version['date']}, Delta: {delta}")
+
+        else:
+            days = 0
+
+
+        # 修改mobileconfig文件
+        if "delay" in version and version["delay"] >= 0:
+            mobileconfig = template.replace("{DELAYPERIOD}", str(version["delay"]))
+            mobileconfig = mobileconfig.replace("{NAME}", str(version["name"]))
+
+            # 保存mobileconfig文件
+            filename = f"./{version['name']} —剩余{days}天.mobileconfig"
+            with open(filename, "w") as f:
+                f.write(mobileconfig)
+
+            # 将json字符串解析成字典
+            # 修改字典中的值
+            url = urlparse(template_url)._replace(path=filename).geturl()
+            version["url"] = url
+
+    # 将字典转换回json字符串并保存到文件中
+    with open("yanchi.json", "w") as f:
+        f.write(json.dumps(response))
+
+
+print("生成yanchi.json文件成功！")
