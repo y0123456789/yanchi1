4c723c65c31c0c86f5d2e326b34078a7eb7edb8d
y0123456789 <52168408+y0123456789@users.noreply.github.com>
2023-05-14 17:57:27 +0800

    Update yanchi.py

--- yanchi.py
+++ yanchi.py
@@ -10,32 +10,39 @@ url = "https://y0123456789.github.io/check-pallas/minified-v3.json"
 response = requests.get(url).json()
 
 # 获取mobileconfig模板文件
-template_url = "https://github.com/y0123456789/yanchi/blob/main/template.mobileconfig"
+template_url = "https://raw.githubusercontent.com/y0123456789/yang/main/deploy/template.mobileconfig"
 template = requests.get(template_url).text
 
 # 循环修改模板文件并保存mobileconfig文件
-for minor_version in response["minorVersions"]:
-    # 获取天数差
-    if minor_version["date"]:
-        delta = datetime.now(timezone.utc) - datetime.fromisoformat(minor_version["date"].replace("Z", "+00:00"))
-        days = delta.days
-    else:
-        days = 0
+# 循环获取版本名称和更新时间
+for name, versions in response.items():
+    print(f"{name}:")
+    for version in versions:
+        if isinstance(version, dict) and version.get('date') is not None and version.get('delay') is not None and version.get('delay') >= 0:
+            delta = datetime.now(timezone.utc) - datetime.fromisoformat(version["date"].replace("Z", "+00:00"))
+            days = delta.days
+            print(f"Name: {version['name']}, Date: {version['date']}, Delta: {delta}")
 
-    # 修改mobileconfig文件
-    mobileconfig = template.replace("{DELAYPERIOD}", str(minor_version["delay"]))
+        else:
+            days = 0
 
-    # 保存mobileconfig文件
-   filename = "/path/to/" + f"{minor_version['name']} —剩余{days}天.mobileconfig"
-   with open(filename, "w") as f:
-    f.write(mobileconfig)
+        # 修改mobileconfig文件
+        if "delay" in version and version["delay"] >= 0:
+            mobileconfig = template.replace("{DELAYPERIOD}", str(version["delay"]))
 
-    # 添加mobileconfig文件的url到json文件中
-    url = urlparse(template_url)._replace(path=filename).geturl()
-    minor_version["url"] = url
+            # 保存mobileconfig文件
+            filename = f"./{version['name']} —剩余{days}天.mobileconfig"
+            with open(filename, "w") as f:
+                f.write(mobileconfig)
 
-# 保存修改后的json文件
-with open("/path/to/yanchi.json", "w") as f:
-    json.dump(response, f)
+            # 将json字符串解析成字典
+            # 修改字典中的值
+            url = urlparse(template_url)._replace(path=filename).geturl()
+            version["url"] = url
+
+    # 将字典转换回json字符串并保存到文件中
+    with open("yanchi.json", "w") as f:
+        f.write(json.dumps(response))
+
 
 print("生成yanchi.json文件成功！")
